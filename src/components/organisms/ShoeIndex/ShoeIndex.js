774b86d3b4865089a76f8d65b1fc72e5062df13f
Agata Zurek <agata.w.zurek@gmail.com>
2021-07-03 23:47:31 -0700

    solution to ShoeIndex

--- src/components/organisms/ShoeIndex/ShoeIndex.js
+++ src/components/organisms/ShoeIndex/ShoeIndex.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/macro';
 
+import { COLORS, WEIGHTS } from '../../../constants';
+
+import ShoeGrid from '../../organisms/ShoeGrid';
+import ShoeSidebar from '../../organisms/ShoeSidebar';
+
 const ShoeIndex = () => {
-  return 'ShoeIndex';
+  const [activeCategory, setActiveCategory] = useState('');
+
+  return (
+    <Wrapper>
+      <StyledShoeSidebar
+        activeCategory={activeCategory}
+        setActiveCategory={setActiveCategory}
+      />
+      <StyledShoeGrid activeCategory={activeCategory} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: var(--horizontalPadding);
+`;
+
+const StyledShoeSidebar = styled(ShoeSidebar)`
+  flex: 1;
+`;
+
+const StyledShoeGrid = styled(ShoeGrid)`
+  flex: 3;
+`;
 
 export default ShoeIndex;
