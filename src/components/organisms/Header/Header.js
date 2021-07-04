38b1b29eb63496e8fd4a574ee60a2a6589888744
Agata Zurek <agata.w.zurek@gmail.com>
2021-07-03 19:39:44 -0700

    update: style header

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { NAV } from '../../../data';
+import { COLORS, WEIGHTS } from '../../../constants';
+
+import Logo from '../../atoms/Logo';
 import SuperHeader from '../SuperHeader';
 
 const Header = () => {
   return (
     <Wrapper>
-      <SuperHeader></SuperHeader>
+      <SuperHeader />
+      <MainNav>
+        <LogoLink />
+        {NAV.map(link => (
+          <Link href={`/${link}`}>{link}</Link>
+        ))}
+      </MainNav>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div``;
+
+const MainNav = styled.nav`
+  align-items: baseline;
+  border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  gap: 48px;
+  height: 72px;
+  justify-content: center;
+  padding: 21px var(--horizontalPadding) 23px;
+`;
+
+const LogoLink = styled(Logo)`
+  margin-right: auto;
+`;
+
+const Link = styled.a`
+  color: ${COLORS.gray[900]};
+  font-size: ${18 / 16}rem;
+  font-weight: ${WEIGHTS.medium};
+  text-decoration: none;
+  text-transform: uppercase;
+  &:last-of-type {
+    margin-right: auto;
+  }
+  &.active,
+  &:active,
+  &:hover {
+    color: ${COLORS.secondary};
+  }
+`;
 
 export default Header;
